6a6b7dd5a3d6f29952bbc8eb06e67e9d58a6be8c
Adam Taylor <a-taylor@outlook.com>
2017-07-23 15:52:22 +0100

    Build out content structure of site

--- index.php
+++ index.php
@@ -17,31 +17,168 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="High specification office space and accommodation at an affordable price with a high car parking ratio and a growing onsite community.">
 
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,800" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="public/css/style.min.css">
 
 	<script type="text/javascript" src="public/js/head.min.js"></script>
 </head>
 <body>
 
+	<div class="u-row">
+		<div class="u-container u-container--wide">
+
+			<img class="o-logo" src="public/images/lwp-logo.png" alt="Lynch Wood Park, Peterborough" width="" height="">
+
+			<a href="http://www.fi-rem.com/">
+				<img class="o-logo o-logo--right" src="public/images/fi-real-estate-logo.png" alt="Asset managed by FI Real Estate Management" width="" height="">
+			</a>
+		
+		</div>
+	</div>
+
 	<header class="c-header" role="banner">
 
-		
+		<div class="c-header__content">
+			
+			<h1 class="c-header__title">
+				<b>Business</b><br>
+				brought<br>
+				to life
+			</h1>
+
+			<p>Up to 49,500 sq ft of premium office space available in Cambridgeshire</p>
+			
+		</div>
+
+		<div class="c-carousel" data-carousel>
+			<div class="c-carousel__slide">
+				<img class="u-block-image" src="public/images/carousel/1.jpg" alt="A view of Lynch Wood Park" width="" height="">
+			</div>
+
+			<div class="c-carousel__slide">
+				<img class="u-block-image" src="public/images/carousel/1.jpg" alt="A view of Lynch Wood Park" width="" height="">
+			</div>
+
+			<div class="c-carousel__slide">
+				<img class="u-block-image" src="public/images/carousel/1.jpg" alt="A view of Lynch Wood Park" width="" height="">
+			</div>
+		</div>
 		
 	</header>
 
 	<main role="main">
-		<div class="u-container">
+
+		<article class="o-article" role="article">
 			
-			
+			<div class="o-section o-section--dark">
+				<div class="u-container">
+
+				
+					<header>
+						
+						<h2 class="o-title-primary">
+							Lynch Wood<br>
+							<b>Park</b>
+						</h2>
+						
+					</header>
+
+					<img class="u-block-image" src="public/images/main.jpg" alt="A view of Lynch Wood Park" width="" height="">
+
+					<p class="o-article__left-text u-text-emphasis">Lynch Wood Park offers variable floorplates suitable for a range of occupiers.</p>
+
+					<p class="o-article__right-text">High specification office accommodation at an affordable price with a high car parking ratio while the tranquil green landscape, central location and growing onsite community provides businesses the opportunity to thrive.</p>
+
+				</div>
+			</div>
+
+			<section class="o-section" role="section">
+				<div class="u-container">
+
+					<h3 class="o-title-primary">
+						Central<br>
+						Location
+					</h3>
+
+					<div class="u-columns">
+					
+						<div class="u-column u-column--1-2">
+							
+							<p class="u-text-emphasis">Lynch Wood Park is ideally located in the heart of the UK, three miles west of Peterborough Town Centre.</p>
+							<p>With excellent connectivity via road and rail, as well as four international airports within an hour and a half.</p>
+							<p>One of the fastest growing cities in the UK, Peterborough is also one of the most cost-effective business locations, ideal for start-ups, SMEs and leading companies alike.</p>
+							
+						</div>	
+
+						<div class="u-column u-column--1-2">
+							
+							<img class="u-block-image" src="public/images/map.png" alt="A map of the location of Lynch Wood Park" width="" height="">
+
+						</div>
+
+					</div>
+
+				</div>				
+			</section>
+
+			<div class="u-row">
+				<img class="u-block-image" src="public/images/banner.jpg" alt="A view of Lynch Wood Park" width="" height="">
+			</div>
+
+			<section class="o-section o-section--dark o-section--pattern">
+				<div class="u-container">
+
+					<h3 class="o-title-primary">
+						All<br>
+						Enquiries
+					</h3>
+
+					<div class="u-columns">
+						
+						<div class="u-column u-column--1-2">
+
+							<img class="u-block-image-max" src="public/images/savills-logo.png" alt="Savills Logo" width="" height="">
+
+							<p>William Rose</p>
+							<p>01733 344414</p>
+							
+						</div>
+
+						<div class="u-column u-column--1-2">
+
+							<img class="u-block-image-max" src="public/images/cushman-wakefield-logo.png" alt="Cushman &amp; Wakefield Logo" width="" height="">
+
+							<p>Andrew Berry</p>
+							<p>0121 200 2050</p>
+							
+						</div>
+
+					</div>
+				
+				</div>
+			</section>
 		
-		</div>
+		</article>
+		
 	</main>
 
 	<footer class="c-footer" role="contentinfo">
+		<div class="u-container">
 		
-		&copy; <?php echo date('Y'); ?>
+			<div class="c-footer__left">
+				
+				&copy; FI Real Estate <?php echo date('Y'); ?>	
+			
+			</div>
 
+			<div class="c-footer__right">
+				
+				<a href="http://www.fi-rem.com/legals/">Privacy Policy</a>
+				<a href="http://www.core-marketing.co.uk/">Website by Core</a>
+
+			</div>
+
+		</div>
 	</footer>
 
 	<script type="text/javascript" src="public/js/footer.min.js"></script>
