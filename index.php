339025d8d282d3ae1cb9099c756aea7de5255058
Adam Taylor <a-taylor@outlook.com>
2017-07-23 15:00:12 +0100

    Set up project
    Add project colours, basic structure

--- index.php
+++ index.php
@@ -1,61 +1,50 @@
 <?php 
 
 /**
- * The home page of the site.
+ * The landing page of the site.
  */
 
-// Get the basename of the file, i.e. anything before the .php
-// Use that as the pagename for templating purposes.
-$page = basename(__FILE__);
-$page = substr( $page, 0, strrpos($page, '.') );
-
 ?><!DOCTYPE html>
-<!--[if IE 7]>
-<html class="ie ie7 no-js" lang="en">
+<!--[if IE 9]>
+<html class="ie ie9 no-js" lang="en">
 <![endif]-->
-<!--[if IE 8]>
-<html class="ie ie8 no-js" lang="en">
-<![endif]-->
-<!--[if !(IE 7) | !(IE 8) ]><!-->
+<!--[if !(IE 9) ]><!-->
 <html class="no-js" lang="en">
 <!--<![endif]-->
 <head>
-	<title><?php echo $page ?> | SITE_NAME</title>
+	<title>49,500 sq ft Office Space | Lynch Wood Park</title>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="DESCRIPTION">
+	<meta name="description" content="High specification office space and accommodation at an affordable price with a high car parking ratio and a growing onsite community.">
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="css/style.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,800" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="public/css/style.min.css">
 
-	<?php 
-  	/**
-  	 * Replace the .no-js class on the <html> element if JS enabled. 
-  	 * This runs in the header because UI may depend on it and we don't want a FOUC.
-  	 * @see https://en.wikipedia.org/wiki/Flash_of_unstyled_content
-  	 *
-  	 * Also minified, as we will not need to modify in the future.
-  	 */
-  ?>
-  <script type="text/javascript">document.documentElement.className=document.documentElement.className.replace("no-js","js");</script>
-
-	<!--[if IE]>
-	  <script src="js/html5shiv.js"></script>
-  <![endif]-->
+	<script type="text/javascript" src="public/js/head.min.js"></script>
 </head>
 <body>
 
-	<?php include_once 'header.php'; ?>
+	<header class="c-header" role="banner">
 
-	<main class="content" role="main">
-		<div class="container">
+		
+		
+	</header>
+
+	<main role="main">
+		<div class="u-container">
 			
 			
 		
 		</div>
 	</main>
 
-	<?php include_once 'footer.php'; ?>
+	<footer class="c-footer" role="contentinfo">
+		
+		&copy; <?php echo date('Y'); ?>
+
+	</footer>
+
+	<script type="text/javascript" src="public/js/footer.min.js"></script>
 
 </body>
 </html>
