0a2bf778c3602c6b509836ff77d60971652a2ac2
Adam Taylor <a-taylor@outlook.com>
2017-07-29 13:04:47 +0100

    Switch to animation library for silky smooth performance

---
+++ src/js/footer/_ScrollMagic.animations.js
@@ -0,0 +1,113 @@
+// Define Animations for ScrollMagic
+
+( function( $, window, document, undefined ) {
+	
+	/**
+	 * Define drop in animations.
+	 * Can be injected with dependancy injection 
+	 * when Animator is initialised.
+	 *
+	 * All animations should adhere to the interface
+	 * (Object) getTween()     Return a TimelineMax object defining the tween of the animation.
+	 * 		@param {DOMElement} element A DOM node to animate. 
+	 * (Object) getOptions()   Return an options object defining the tween.
+	 */
+
+	/**
+	 * Define slide in from bottom animation
+	 */
+	var SlideInBottom = function( element ) {
+		var _ = this;
+
+		_.element = element;
+		_.duration = 1;
+	};
+
+	SlideInBottom.prototype.getTween = function() {
+		var _ = this;
+
+		return new TimelineMax().from(
+			_.element,
+			_.duration,
+			_.getOptions()
+		);
+	};
+
+	SlideInBottom.prototype.getOptions = function() {
+		return {
+			opacity: 0,
+			y: "+=200"
+		};
+	};
+
+	/**
+	 * Define slide in from left animation
+	 */
+	var SlideInLeft = function( element ) {
+		var _ = this;
+
+		_.element = element;
+		_.duration = 2;
+		_.ease = Expo.easeOut;
+	};
+
+	SlideInLeft.prototype.getTween = function() {
+		var _ = this;
+
+		return new TimelineMax().from(
+			_.element,
+			_.duration,
+			_.getOptions()
+		);
+	};
+
+	SlideInLeft.prototype.getOptions = function() {
+		var _ = this;
+
+		return {
+			opacity: 0,
+			x: "-=200",
+			ease: _.ease
+		};
+	};
+	
+	/**
+	 * Define slide in from left animation
+	 */
+	var SlideInRight = function( element ) {
+		var _ = this;
+
+		_.element = element;
+		_.duration = 2;
+		_.ease = Expo.easeOut;
+	};
+
+	SlideInRight.prototype.getTween = function() {
+		var _ = this;
+
+		return new TimelineMax().from(
+			_.element,
+			_.duration,
+			_.getOptions()
+		);
+	};
+
+	SlideInRight.prototype.getOptions = function() {
+		var _ = this;
+
+		return {
+			opacity: 0,
+			x: "+=200",
+			ease: _.ease
+		};
+	};
+	
+
+	// Expose globally
+	window.ScrollMagicAnimations = {
+		SlideInBottom: SlideInBottom,
+		SlideInLeft: SlideInLeft,
+		SlideInRight: SlideInRight
+	};
+
+} )( jQuery, window, document );
--- src/js/footer/z-kickstart.js
+++ src/js/footer/z-kickstart.js
@@ -23,22 +23,47 @@
 	/**
 	 * Initialise animation on scroll
 	 */
-	var $animatedElements = $('[data-animate]');
+	var $animatedElements = $('[data-animate]'),
+			animationController = new ScrollMagic.Controller();
 
 	$animatedElements.each( function( index, node ) {
 
 		var $this = $(node),
-				type = $this.attr( 'data-animate' );
+				type = $this.data( 'animate' ),
+				offset = $this.data( 'offset' ),
+				animation,
+				scene;
 
-		if ( 'fadeIn' === type ) {
-			$this.Animator = new Animator( $this, new Animations.FadeIn() );
-		} else if ( 'slideInLeft' === type ) {
-			$this.Animator = new Animator( $this, new Animations.SlideIn('left') );
-		} else if ( 'slideInRight' === type ) {
-			$this.Animator = new Animator( $this, new Animations.SlideIn('right') );
-		} else {
-			console.log( 'No animator attached for: ', node );
+		if ( undefined === offset ) {
+			offset = 0;
 		}
+
+		switch ( type ) {
+			case 'slideInBottom':
+				animation = new ScrollMagicAnimations.SlideInBottom( node );
+				break;
+
+			case 'slideInLeft':
+				animation = new ScrollMagicAnimations.SlideInLeft( node );
+				break;
+
+			case 'slideInRight':
+				animation = new ScrollMagicAnimations.SlideInRight( node );
+				break;
+
+			default:
+				break;
+		}
+
+		scene = new ScrollMagic.Scene( {
+			triggerElement: node,
+			// Only play once on scroll down, not on scroll up
+			reverse: false
+		} )
+		.triggerHook( 1 )
+		.offset( offset )
+		.setTween( animation.getTween() )
+		.addTo( animationController );
 
 	} );
 
