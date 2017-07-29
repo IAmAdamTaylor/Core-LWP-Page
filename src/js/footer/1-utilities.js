4311f82536a2c95e38f2062fefb3a3686d675a18
Adam Taylor <a-taylor@outlook.com>
2017-07-29 13:04:57 +0100

    Remove unused functions

--- src/js/footer/1-utilities.js
+++ src/js/footer/1-utilities.js
@@ -1,12 +1,5 @@
 // Utility JavaScript functions
 
-// Polyfill
-if (!Array.isArray) {
-  Array.isArray = function(arg) {
-    return Object.prototype.toString.call(arg) === '[object Array]';
-  };
-}
-
 // mobile Safari reports wrong values on offset()
 // http://dev.jquery.com/ticket/6446
 if ( /webkit.*mobile/i.test(navigator.userAgent)) {
@@ -20,75 +13,4 @@ if ( /webkit.*mobile/i.test(navigator.userAgent)) {
         return result;
       };
   })(jQuery);
-}
-
-/**
- * Get the true window width, without scrollbars.
- * @return integer The window width in px.
- */
-function windowWidth() {
-  // Temporarily disable scrollbar
-  $('body').css( {
-    overflow: 'hidden',
-  } );
-
-  // Get window width
-  var w = $(window).width();
-
-  // Remove styles
-  $('body').css( {
-    overflow: '',
-  } );
-
-  return w;
-}
-
-/**
- * Get the window height.
- * @return integer The window height in px.
- */
-function windowHeight() {
-  return $(window).height();
-}
-
-function throttle(fn, threshold, scope) {
-  threshold = threshold || 250;
-  var last,
-      deferTimer;
-  return function () {
-    var context = scope || this;
-
-    var now = +new Date(),
-        args = arguments;
-    if (last && now < last + threshold) {
-      // hold on to it
-      clearTimeout(deferTimer);
-      deferTimer = setTimeout(function () {
-        last = now;
-        fn.apply(context, args);
-      }, threshold);
-    } else {
-      last = now;
-      fn.apply(context, args);
-    }
-  };
-}
-
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
 }
